3a216d301dad12e6a18930cf5666fe5aa6e88a9d
Sadam Agashirinov <sadam.agashirinov@gmail.com>
2021-09-25 15:11:06 +0300

    Топорный метод решения задачи

---
+++ Fibonachi/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Fibonachi
+{
+    class Program
+    {
+        private static Stopwatch stopwatch = new Stopwatch();
+        
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                stopwatch.Start();
+                new Fibonachi().Run(n: i);
+                stopwatch.Stop();
+                Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms.");
+            }
+        }
+    }
+
+    class Fibonachi 
+    {
+        /// <summary>
+        /// Найти число Фибоначи для n элеметнов
+        /// </summary>
+        public void Run(int n)
+        {
+            Console.WriteLine($"{n}: {Find(n)}");
+        }
+
+        private int Find(int n)
+        {
+            if (n < 2)
+                return n;
+            return Find(n - 1) + Find(n - 2);
+        }
+    }
+}
