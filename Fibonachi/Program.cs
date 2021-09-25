9ecf25975ebdbc64d5dc087c9101bf89540031e3
Sadam Agashirinov <sadam.agashirinov@gmail.com>
2021-09-25 15:30:44 +0300

    Улучшение скорости работы алгоритмы путем сохранения промежуточных результатов.

--- Fibonachi/Program.cs
+++ Fibonachi/Program.cs
@@ -1,26 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace Fibonachi
 {
     class Program
     {
-        private static Stopwatch stopwatch = new Stopwatch();
+        private static readonly Stopwatch _stopwatch = new Stopwatch();
         
         static void Main(string[] args)
         {
             for (var i = 0; i < 100; i++)
             {
-                stopwatch.Start();
+                _stopwatch.Start();
                 new Fibonachi().Run(n: i);
-                stopwatch.Stop();
-                Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms.");
+                _stopwatch.Stop();
+                Console.WriteLine($"{_stopwatch.ElapsedMilliseconds} ms.");
             }
         }
     }
 
-    class Fibonachi 
+    class Fibonachi
     {
+        private Dictionary<int, long> cache = new Dictionary<int, long>();
+
         /// <summary>
         /// Найти число Фибоначи для n элеметнов
         /// </summary>
@@ -29,11 +32,21 @@ namespace Fibonachi
             Console.WriteLine($"{n}: {Find(n)}");
         }
 
-        private int Find(int n)
+        private long Find(int n)
         {
             if (n < 2)
                 return n;
-            return Find(n - 1) + Find(n - 2);
+            if (cache.ContainsKey(n))
+            {
+                cache.TryGetValue(n, out var value);
+                return value;
+            }
+            else
+            {
+                var result = Find(n - 1) + Find(n - 2);
+                cache.Add(n, result);
+                return result;
+            }
         }
     }
 }
