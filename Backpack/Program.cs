bc48918b0d274c2abbaa5aeae2ea7776edc2217a
Sadam Agashirinov <sadam.agashirinov@gmail.com>
2021-09-26 11:42:09 +0300

    Задача наполнение рюкзака

---
+++ Backpack/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+//Задача на наполнение рюкзака
+//Использовать жадный алгоритм
+
+namespace Backpack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
